657d7dfbd830eea592a7c6003d523e08e246c662
Abdul_Rehman_Gujjar <abdulRehmangujjar110@gmail.com>
2023-03-11 00:47:17 +0500

    work on home page banner and content and navbar and home page banner and content responsive in 768px width

---
+++ homePageFuntions.js
@@ -0,0 +1,9 @@
+let navTop = document.querySelector(".navResicon");
+
+navTop.addEventListener("click", () => navF());
+
+let navbarTop = document.querySelector(".navbarMaindiv");
+
+let navF = () => {
+    navbarTop.classList.toggle("activeNavbar");
+}
